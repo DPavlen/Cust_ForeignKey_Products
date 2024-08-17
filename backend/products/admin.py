a13331f1950816af02d4b5b3170469e33e611218
Dmitry Pavlenko <125051658+DPavlen@users.noreply.github.com>
2024-08-17 17:10:13 +0300

    Add models Attr, Product,  ProductAttr + add admin. Add in settings - Вынесение секретов в .env.

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import Attr, Product, ProductAttr
+
+
+@admin.register(Attr)
+class AttrAdmin(admin.ModelAdmin):
+    """
+    Административный интерфейс для управления атрибутами.
+    """
+    list_display = ("id", "name",)
+    search_fields = ("id", "name",)
+    list_filter = ("id", "name",)
+    list_editable = ("name",)
+    empty_value_display = "-пусто-"
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Административный интерфейс для управления продуктами.
+    """
+    def get_attrs(self, obj):
+        return " ; ".join([attr.name for attr in obj.attrs.all()])
+
+    get_attrs.short_description = "Attributes"
+
+    list_display = ("id", "name", "get_attrs")
+    search_fields = ("id", "name")
+    list_filter = ("id", "name")
+    list_editable = ("name",)
+    empty_value_display = "-пусто-"
+
+
+@admin.register(ProductAttr)
+class ProductAttrAdmin(admin.ModelAdmin):
+    """
+    Административный интерфейс для управления связи атрибутов и продукта.
+    """
+    list_display = ("id", "attr", "product", "value")
+    search_fields = ("id", "attr", "product", "value")
+    list_filter = ("id", "attr", "product", "value")
+    # list_editable = ("name",)
+    empty_value_display = "-пусто-"
--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Attr(models.Model):
+    """Модель атрибутов."""
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "Атрибут"
+        verbose_name_plural = "Атрибуты"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """Модель продукта."""
+    name = models.CharField(max_length=100)
+    attrs = models.ManyToManyField("Attr", through="ProductAttr")
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        return self.name
+
+
+class ProductAttr(models.Model):
+    """Промежуточная Модель связи атрибутов и продукта ."""
+    attr = models.ForeignKey("Attr", on_delete=models.CASCADE)
+    product = models.ForeignKey("Product", on_delete=models.CASCADE)
+    value = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "Атрибут и Продукт"
+        verbose_name_plural = "Атрибуты и Продукты"
+
+    def __str__(self):
+        return (f"Продукт {self.product} с атрибутами: {self.attr}"
+                f" значениями: {self.value} ")
+
+# class UniqueProduct(models.Model):
+#     """Модель уникального продукта."""
+#     product = models.CustomForeignKey(Product, on_delete=models.PROTECT)
+#     attr = models.ForeignKey(ProductAttr, on_delete=models.PROTECT)
